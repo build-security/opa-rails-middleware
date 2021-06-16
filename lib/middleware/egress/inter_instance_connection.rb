b05ca60e9f3a489beb8e4ed2fc4a9f00677b7a37
Yash Tewari <yashtewari1996@gmail.com>
2021-06-16 17:03:44 +0530

    First draft

---
+++ lib/middleware/egress/inter_instance_connection.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'aws-sdk-ec2'
+require 'base64'
+require 'faraday'
+
+module Middleware
+    module Egress
+        @@awsIdentityDocument = nil
+        @@awsSignature = nil
+
+        def self.getAwsMetadata(metadata_options)
+            # TODO(yashtewari): Make thread-safe
+            if @@awsIdentityDocument.nil?
+                ec2_metadata = Aws::EC2Metadata.new(metadata_options)
+
+                Base64.encode64(ec2_metadata.get('/latest/dynamic/instance-identity/rsa2048'))
+            end
+        end
+
+        class InjectAwsIdentityDocument < Faraday::Middleware
+            def initialize(app, metadata_options = {})
+                super(app)
+
+                @metadata_options = metadata_options
+                @app = app
+            end
+
+            def on_request(env)
+                signed_data = Middleware::Egress::getAwsMetadata(@metadata_options)
+                
+                env.request_headers['Aws-Signed-Metadata'] = signed_data
+            end
+        end
+
+        class AwsInterInstanceConnection < Faraday::Connection
+            def initialize(url = nil, options = nil)
+                super(url, options) do |conn|
+                    conn.use InjectAwsIdentityDocument
+                end
+            end
+        end
+    end
+end
---
+++ lib/middleware/ingress/policy_decision_point.rb
@@ -0,0 +1,214 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'faraday'
+require 'openssl'
+require 'base64'
+
+require 'aws-sdk-ec2'
+require 'aws-sdk-iam'
+
+module Middleware
+    module Ingress
+        ##
+        # Ingress middleware that connects to a PDP to request authz decisions.
+        class PolicyDecisionPoint
+
+            ##
+            # Initializes the PolicyDecisionPoint middleware.
+            #
+            # @param port [Integer] The port at which the PDP serves authz decisions.
+            # @param hostname [String] The host at which the PDP serves authz decisions.
+            # @param policy_path [String] The full path to policy (including package and rule) that makes the authz decision.
+            # @param read_timeout_milliseconds [Integer] Timeout for reading packets from server.
+            # @param connection_timeout_milliseconds [Integer] Timeout for establishing connection to server.
+            # @param retry_max_attempts [Integer] Number of retries to server before giving up.
+            # @param retry_backoff_milliseconds [Integer] Initial wait time before retry, doubles for every retry.
+            def initialize(app,
+                port: 8181,
+                hostname: 'http://localhost', 
+                policy_path: '/authz/allow',
+                read_timeout_milliseconds: 5000,
+                connection_timeout_milliseconds: 5000,
+                retry_max_attempts: 2,
+                retry_backoff_milliseconds: 250)
+
+                @port = port
+                @hostname = hostname
+                @policy_path = policy_path
+                @read_timeout_milliseconds = read_timeout_milliseconds
+                @connection_timeout_milliseconds = connection_timeout_milliseconds
+                @retry_max_attempts = 2
+                @retry_backoff_milliseconds = retry_backoff_milliseconds
+
+                @pdp_endpoint = endpoint()
+
+                @client = Faraday.new(
+                    url: @pdp_endpoint,
+                    request: {
+                        read_timeout: @read_timeout_milliseconds,
+                        open_timeout: @connection_timeout_milliseconds,
+                    }
+                ) do |conn|
+                    conn.request(:retry, max:2, interval: @retry_backoff_milliseconds/1000, backoff_factor: 2)
+                end
+
+                @ec2 = Aws::EC2::Client.new
+                @iam = Aws::IAM::Client.new
+                @app = app
+            end
+
+            ##
+            # Invoked for incoming requests; calls the PDP with request context to make decision.
+            #
+            # @raise [Middleware::Ingress::AuthzError] if the request fails authorization.
+            def call(env)
+                headers = ActionDispatch::Http::Headers.from_hash(env)
+
+                signed_data = Base64.decode64(headers['HTTP_AWS_SIGNED_METADATA'])
+                certificate = File.read(File.join(File.dirname(__FILE__), 'aws_imds_cert.pem'))
+
+                metadata = verify(signed_data, certificate)
+                iam_instance_profile = get_instance_profile(metadata['instanceId'])
+
+                body = input(env, headers, metadata, iam_instance_profile).to_json
+
+                response = @client.post('', body, 'Content-Type' => 'application/json')
+
+                if not response.success?
+                    raise AuthzError.new(
+                        StandardError.new("Unexpected response #{response.status} from decision endpoint #{@pdp_endpoint}")
+                    )
+                end
+
+                if JSON.parse(response.body).with_indifferent_access['result'] != true
+                    raise AuthzError.new(
+                        StandardError.new("Request was not authorized by decision endpoint #{@pdp_endpoint}")
+                    )
+                end
+
+            rescue Faraday::Error => error
+                raise AuthzError.new(error)
+            else
+                # If no exception is raised, pass the request to the next middleware.
+                @app.call(env)
+            end
+
+            ##
+            # Constructs the endpoint to which PDP authz requests are sent.
+            #
+            # @return [String] the endpoint
+            def endpoint
+                if not @hostname.include? '://'
+                    @hostname = 'http://' + @hostname
+                end
+
+                @hostname = @hostname.delete_suffix('/')
+
+                if @port.respond_to?(:to_s)
+                    @port = @port.to_s
+                end
+
+                if not @port.start_with?(':')
+                    @port = ':' + @port
+                end
+
+                if not @policy_path.start_with?('/')
+                    @policy_path = '/' + @policy_path
+                end
+
+                @hostname + @port + '/v1/data' + @policy_path
+            end
+
+            ##
+            # Verifies PKCS7 signed data
+            #
+            # @param pkcs7_signed_data [String] PKCS7 signed data structure with attached data
+            # @param certificate [String] certificate provided by signer
+            # 
+            # @return data [Hash] content from the PKCS7 signed data structure
+            def verify(pkcs7_signed_data, certificate)
+                cert = OpenSSL::X509::Certificate.new(certificate)
+                pkcs7 = OpenSSL::PKCS7.new(File.read(pkcs7_signed_data))
+                store = OpenSSL::X509::Store.new()
+                
+                unless pkcs7.verify([cert], store, pkcs7.data, OpenSSL::PKCS7::NOVERIFY)
+                    raise StandardError.new("Could not verify incoming request signature")
+                end
+
+                JSON.parse(pkcs7.data)
+            end
+
+            ##
+            # Fetches IAM Instace Profile information associated with the EC2 instance.
+            #
+            # @param ec2_instance_id [String]
+            #
+            # @return instance_profile [Hash] details of IAM Instance Profile associated with EC2 instance, or nil
+            def get_instance_profile(ec2_instance_id)
+                instances =  client.describe_instances({instance_ids: [ec2_instance_id]})
+                ec2_instance_profile = instances.reservations[0].instances[0].iam_instance_profile
+
+                unless ec2_instance_profile.nil?
+                    instance_profile_name = instance_profile.arn.split('/')[-1]
+                    return iam.get_instance_id(instance_profile_name).to_h
+                end
+
+                nil
+            end
+
+            ##
+            # Constructs the request context required to make authz decision, which is sent to the PDP.
+            # Header values received in the request are normalized. See NOTE below for examples.
+            #
+            # @return [Hash] the request context
+            def input(env, headers, metadata, iam_instance_profile)
+                req = Rack::Request.new(env)
+
+                {
+                    'input': {
+                        'request': {
+                            'scheme': req.scheme,
+                            'method': req.request_method,
+                            'path': req.path,
+                            'query':  Rack::Utils.parse_nested_query(req.query_string),
+
+                            # NOTE: Rack normalizes all headers by adding prefixes, making uppercase and using underscores.
+                            # Examples
+                            #   Content-Type -> CONTENT_TYPE
+                            #   Accept -> HTTP_ACCEPT
+                            #   Custom-Header -> HTTP_CUSTOM_HEADER
+                            #
+                            # To separate these request headers from other environment headers added by Rails/Rack,
+                            # the following prefix matching is performed.
+                            'headers': headers.to_h.select { |k,v|
+                                ['HTTP','CONTENT','AUTHORIZATION'].any? { |s| k.to_s.starts_with? s }
+                            },
+                        },
+                        'source': {
+                            'ipAddress': req.host,
+                            'port': req.port,
+                        },
+                        'destination': {
+                            'ipAddress': req.server_name,
+                            'port': req.server_port,
+                        },
+                        'aws': {
+                            'imds': metadata,
+                            'iam_instance_profile': iam_instance_profile,
+                        }
+                    }
+                }
+            end
+        end
+
+        ##
+        # Represents an authorization attempt failure.
+        class AuthzError < StandardError
+            def initialize(e = nil)
+                super e
+                set_backtrace e.backtrace if e
+            end
+        end
+    end
+end
