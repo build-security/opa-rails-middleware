01b90973b69151cd4b520255552f01944f39ad5c
Yash Tewari <yashtewari1996@gmail.com>
2021-06-29 16:57:24 +0530

    Allow hosts; fix demo client outputs (#2)

--- example/config/environments/development.rb
+++ example/config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow all hosts.
+  config.hosts.clear
 end
--- example/egress.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'lib/middleware/egress/inter_instance_connection'
-
-conn = Middleware::Egress::AwsInterInstanceConnection.new
-
-conn.get('http://google.com')
