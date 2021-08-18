1964a1eb0f62ccac8283d4408b253ceddf90c85a
Yash Tewari <yashtewari1996@gmail.com>
2021-08-18 14:38:42 +0530

    BS-4019: Gemspec changes and example removed. (#11)
    * BS-4019: Don't add example to Gem.
    
    * BS-4019: Remove outdated/broken example.

--- example/config/initializers/twirp_rails.rb
+++
@@ -1,4 +0,0 @@
-Twirp::Rails.configuration do |c|
-    # Modify the path below if you locates handlers under the different directory.
-    c.handlers_path = Rails.root.join('app', 'controllers')
-  end
